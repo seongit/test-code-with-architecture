b734c1ab58915cbf664e1569904d0813524be3c2
SeongEun Kim <seongeunk102@gmail.com>
2024-06-19 21:46:45 +0900

    PostServiceTest 작성

---
+++ src/test/java/com/example/demo/service/PostServiceTest.java
@@ -0,0 +1,70 @@
+package com.example.demo.service;
+
+
+import com.example.demo.model.dto.PostCreateDto;
+import com.example.demo.model.dto.PostUpdateDto;
+import com.example.demo.repository.PostEntity;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestPropertySource("classpath:test-application.properties")
+@SqlGroup({
+        @Sql(value = "/sql/post-service-test-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Test
+    void getById로_존재하는_게시물을_내려준다() {
+        // given
+        // when
+        PostEntity result = postService.getById(1);
+
+        // then
+        assertThat(result.getContent()).isEqualTo("helloworld");
+        assertThat(result.getId()).isEqualTo(1);
+    }
+
+
+    @Test
+    void create로_게시글을_생성한다() {
+        // given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .writerId(1)
+                .content("foobar")
+                .build();
+        // when
+        PostEntity result = postService.create(postCreateDto);
+
+        // then
+        assertThat(result.getId()).isNotNull();
+        assertThat(result.getContent()).isEqualTo("foobar");
+        assertThat(result.getCreatedAt()).isGreaterThan(0L);
+    }
+
+    @Test
+    void update로_게시글을_수정한다() {
+        // given
+        PostUpdateDto dto = PostUpdateDto.builder()
+                .content("hello world :)")
+                .build();
+
+        // when
+        postService.update(1, dto);
+
+        // then
+        PostEntity result = postService.getById(1);
+        assertThat(result.getContent()).isEqualTo("hello world :)");
+    }
+
+}

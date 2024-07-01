077736bee89cf7685e06f520386981cc2ec1fc13
김성은 <se.kim@okestro.com>
2024-07-01 12:57:13 +0900

    PostResponseTest 테스트 코드 추가

--- src/test/java/com/example/demo/post/controller/response/PostResponseTest.java
+++ src/test/java/com/example/demo/post/controller/response/PostResponseTest.java
@@ -1,14 +1,32 @@
 package com.example.demo.post.controller.response;
 
+import com.example.demo.post.domain.dto.Post;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PostResponseTest {
     @Test
     void Post로_응답객체를_생성할_수_있다() {
     	// given
-
+        Post post = Post.builder()
+                .writer(User.builder()
+                        .id(1L)
+                        .email("se.kim@naver.com")
+                        .nickname("seong")
+                        .address("Seoul")
+                        .status(UserStatus.ACTIVE)
+                        .certificationCode(UUID.randomUUID().toString())
+                        .build())
+                .content("test")
+                .build();
     	// when
-
+        PostResponse postResponse = PostResponse.from(post);
     	// then
+        assertThat(postResponse.getContent()).isEqualTo("test");
     }
 }

1ebd085224c5ddb7397125c9d6dc35ba96b37d68
김성은 <se.kim@okestro.com>
2024-07-01 12:53:37 +0900

    PostCreate 테스트 코드 추가

--- src/test/java/com/example/demo/post/domain/PostTest.java
+++ src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,16 +1,39 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.post.domain.dto.Post;
+import com.example.demo.post.domain.dto.PostCreate;
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class PostTest {
 
     @Test
     void PostCreate으로_게시물을_만들_수_있다() {
     	// given
+        PostCreate postCreate = PostCreate.builder()
+                .writerId(1)
+                .content("helloWorld")
+                .build();
+
+        User writer =  User.builder()
+                .email("se.kim@naver.com")
+                .nickname("seong")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode(UUID.randomUUID().toString())
+                .build();
 
     	// when
+        Post post = Post.from(writer,postCreate);
 
     	// then
+        assertThat(post.getContent()).isEqualTo("helloWorld");
+        assertThat(post.getWriter().getEmail()).isEqualTo("se.kim@naver.com");
     }
 
 
